8344d85e1867e2eade44ddd0f1339efa3b7e28a6
Андрей Кравчун <andrey@kravchun.ru>
2022-06-29 03:04:49 +0300

    Внесены правки после 1-ого рвеью

--- homework.py
+++ homework.py
@@ -1,7 +1,4 @@
-import logging
-import os
-import requests
-import time
+import logging, os, requests, time
 
 from dotenv import load_dotenv
 from http import HTTPStatus
@@ -9,9 +6,6 @@ from telegram import Bot
 
 load_dotenv()
 
-logging.basicConfig(
-    format='%(asctime)s - %(levelname)s - %(message)s')
-handler = logging.StreamHandler
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -26,86 +20,91 @@ HOMEWORK_STATUSES = {
 }
 
 
-def send_message(bot, message):
+def send_message(bot, message) -> None:
     """Отправляет готовое сообщение."""
-    logging.info('Сообщение успешно отправлено')
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        response = bot.send_message(TELEGRAM_CHAT_ID, message)
+        if response.message_id:
+            logging.info('Сообщение успешно отправлено')
+    except Exception:
+        raise Exception('Сообщение в бот не было отправлено')
 
 
-def get_api_answer(current_timestamp):
+def get_api_answer(current_timestamp) -> dict:
     """Делает API-запрос и возвращает полученный ответ в нужном формате."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    if requests.get(
-            url=ENDPOINT,
-            headers=HEADERS,
-            params=params
-    ).status_code == HTTPStatus.OK:
+    try:
         response = requests.get(url=ENDPOINT, headers=HEADERS, params=params)
-        return response.json()
-    else:
-        logging.error('Эндпоинт недоступен')
-        raise ValueError
+        if response.status_code == HTTPStatus.OK:
+            return response.json()
+    except Exception:
+        raise Exception(f'Эндпоинт недоступен. URL: {ENDPOINT}.'
+                        f'Заголовки: {HEADERS}. Параметры: {params}')
 
 
-def check_response(response):
+def check_response(response) -> dict:
     """Проверяет ответ на соответствие требованиям.
     и возвращает необходимый элемент.
     """
+    logging.info('Начали проверять ответ от сервера')
     if (
-        (type(response['homeworks']) == list)
-        and (len(response['homeworks']) != 0)
+        isinstance(response, dict)
+        and 'homeworks' in response
+        and isinstance(response['homeworks'], list)
+        and len(response['homeworks']) != 0
     ):
-        homework = response['homeworks'][0]
-        return homework
-    else:
-        logging.error('Результат не соответствует ожиданиям')
-        raise ValueError
+        if len(response['homeworks']) == 0:
+            logging.debug('Нет обновлений домашних работ')
+        else:
+            homework = response['homeworks'][0]
+            return homework
+    raise TypeError('Результат запроса API не соответствует ожиданиям')
 
 
-def parse_status(homework):
+def parse_status(homework) -> dict:
     """Проверяет элемент на соответствие требованиям.
     и формирует необходимый результат.
     """
-    if 'homework_name' in homework:
+    if ('homework_name' and 'status') in homework:
         homework_name = homework['homework_name']
-    else:
-        logging.error('Отсутствуют ожидаемые ключи в ответе API')
-        raise KeyError
-    homework_status = homework['status']
-    verdict = HOMEWORK_STATUSES[homework_status]
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        homework_status = homework['status']
+        if homework_status in HOMEWORK_STATUSES:
+            verdict = HOMEWORK_STATUSES[homework_status]
+            return (f'Изменился статус проверки работы "{homework_name}".'
+                    f'Новый статус: "{verdict}".')
+        raise Exception('Отсутствуют необходимые ключи в ответе API')
 
 
-def check_tokens():
+def check_tokens() -> bool:
     """Проверяет наличие необходимых переменных окружения."""
-    if (PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID):
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
         return True
-    else:
-        logging.critical('Отсутствуют обязательные переменные окружения')
-        return False
+    raise Exception('Отсутствуют необходимые переменные окружения')
 
 
-def main():
+def main() -> None:
     """Основная логика работы бота."""
     bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    while check_tokens():
+    current_timestamp = 1655286307
+    while True:
         try:
+            check_tokens()
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
             message = parse_status(homework)
             send_message(bot, message)
             current_timestamp = int(time.time())
-            time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logging.error(message)
+        finally:
             time.sleep(RETRY_TIME)
-        else:
-            logging.debug('Отсутствие в ответе новых статусов')
-            return ValueError
 
 
 if __name__ == '__main__':
     main()
+    logging.basicConfig(
+        format='%(asctime)s, %(levelname)s, %(message)s',
+        handlers=logging.StreamHandler
+    )
